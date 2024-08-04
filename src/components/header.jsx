1825d3450e45736683a1d16e2b38c0b5b0d90622
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2024-08-05 00:16:36 +0530

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,88 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+import { UrlState } from "@/context";
+import useFetch from "@/hooks/use-fetch";
+import { logout } from "@/db/apiAuth";
+import { BarLoader } from "react-spinners";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const { user, fetchUser } = UrlState();
+
+  const { loading, fn: fnLogout } = useFetch(logout);
+
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <div className="flex gap-2 justify-center items-center">
+          <Link>
+            <img
+              src="/url-project-logo-transparent.png"
+              className="h-16"
+              alt="URL Project Logo"
+            />
+          </Link>
+          <p className="font-bold text-xl">The URL Project</p>
+        </div>
+        <div>
+          {!user ? (
+            <Button onClick={() => navigate("/auth")}>Login</Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+                <Avatar>
+                  <AvatarImage
+                    className="object-cover"
+                    src={user?.user_metadata?.profile_pic}
+                  />
+                  <AvatarFallback>
+                    {user?.user_metadata?.name.split("")[0]}
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  {user?.user_metadata?.name}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to="/dashboard" className="flex">
+                    <LinkIcon className="mr-2 h-4 w-4" />
+                    My Links
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-red-400">
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span
+                    onClick={() => {
+                      fnLogout().then(() => {
+                        fetchUser();
+                        navigate("/");
+                      });
+                    }}
+                  >
+                    Logout
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>
+      {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+    </>
+  );
+};
+
+export default Header;
